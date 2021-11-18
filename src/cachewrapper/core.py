16b04792cfcaff57f2e8984e8fbb97111c15141d
Carsten Knoll <carsten.knoll@posteo.de>
2021-11-18 14:53:35 +0100

    implement basic wrapping functionality (without caching)

--- src/cachewrapper/core.py
+++ src/cachewrapper/core.py
@@ -1,1 +1,51 @@
-pass
+import inspect
+from ipydex import IPS, activate_ips_on_exception
+
+activate_ips_on_exception()
+
+
+class CacheWrapper:
+    """
+    Wrapper object
+    """
+
+    def __init__(self, obj) -> None:
+        """
+        Create a wrapper
+        """
+
+        self.wrapped_object = obj
+        self.callables = get_all_callables(obj)
+        self._create_wrapped_callables()
+
+    def _create_wrapped_callables(self):
+        for name, obj in self.callables.items():
+            self._cached_func_factory(name, obj)
+
+    def _cached_func_factory(self, name, obj):
+        """
+        create a new callable obj and install it in the namespace of `self`
+        """
+
+        def func(*args, **kwargs):
+            return obj(*args, **kwargs)
+
+        func.__doc__ = f"{obj}wrapped callable '{name}':\n\n {obj.__doc__}"
+        assert getattr(self, name, None) is None
+        setattr(self, name, func)
+
+
+def get_all_callables(obj, include_private=None) -> dict:
+
+    if include_private is None:
+        include_private = []
+    attribute_names = dir(obj)
+    attribute_dict = dict((name, getattr(obj, name)) for name in attribute_names)
+
+    callables = dict(
+        (name, obj)
+        for (name, obj) in attribute_dict.items()
+        if callable(obj) and (not name.startswith("_") or name in include_private)
+    )
+
+    return callables
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,10 +1,68 @@
 import unittest
+import cachewrapper as cw
+
+from ipydex import IPS, activate_ips_on_exception
+
+
+class DummyClass:
+    def __init__(self) -> None:
+        pass
+
+    def _private_method(self):
+        """
+        docstring of _private_method
+        """
+
+    def public_method1(self, arg1, arg2):
+        """
+        docstring of public_method1
+        """
+        return arg1 + arg2
+
+    def public_method2(self, arg1, arg2):
+        """
+        docstring of public_method2
+        """
+        return arg1 + arg2
+
+    def public_method3(self, *args):
+        """
+        docstring of public_method3: only return self
+        """
+        return self
+
+    def staticmethod1(self, arg1, arg2):
+        """
+        docstring of staticmethod1
+        """
+        return arg1 + arg2
+
 
 # noinspection PyPep8Naming
 class TestCore(unittest.TestCase):
     def setUp(self):
         pass
 
-    # mark tests which only work for the "old core"
-    def test_core1(self):
-        pass
+    def test_CW_get_all_functions_and_methods(self):
+
+        instance = DummyClass()
+        cached_instance = cw.CacheWrapper(instance)
+
+        self.assertEqual(len(cached_instance.callables), 4)
+
+    def test_caching(self):
+
+        original_instance = DummyClass()
+        cached_instance = cw.CacheWrapper(original_instance)
+
+        original_callables = cw.get_all_callables(original_instance)
+        new_callables = cw.get_all_callables(cached_instance)
+
+        self.assertEqual(len(original_callables), len(new_callables))
+
+        self.assertIn("docstring of public_method1", cached_instance.public_method1.__doc__)
+
+        res1 = original_instance.public_method3()
+        res2 = cached_instance.public_method3()
+
+        self.assertEqual(res1, res2)
